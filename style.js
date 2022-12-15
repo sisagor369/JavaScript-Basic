8e659b5e64d386d3a6e816392f2a95f60cb587b2
MD Saiful Islam Sagor <54396678+sisagor369@users.noreply.github.com>
2022-12-15 17:09:06 +0600

    Add files via upload

---
+++ style.js
@@ -0,0 +1,8 @@
+// alert('hello');
+function myfun(){
+    document.getElementById('fun').innerHTML =" changed by function";
+}
+let x= 5;
+let y= 6;
+let z= x + y;
+document.getElementById('valuee').innerHTML = "the value of z is " + z;
